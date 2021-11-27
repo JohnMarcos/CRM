dbb8c7da85f738f2541a29905760ffe5a86b12d0
JohnMarcos <john.marcos.videla.r@gmail.com>
2021-11-27 15:01:04 -0300

    generate controllor Prospects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'prospects/index'
   devise_for :users, skip: [:registrations]
 
 # Recrea las rutas de registro de Devise
@@ -11,7 +12,9 @@ Rails.application.routes.draw do
       as: :user_registration do
     get 'cancel'
   end
+root to: 'prospects#index'
+  resource :prospects
 
-  root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
